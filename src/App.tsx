8fa4586c70228396e32c3ef1b6c1b7a2ee230387
flaviozno <flavio_contato@outlook.com>
2021-07-04 17:08:37 -0300

    day2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
+
+
+import { AuthContextProvider } from './context/AuthContext'
+
+
 function App() {
   return (
-   <>
-   </>
-  );
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  )
 }
 
 export default App;
